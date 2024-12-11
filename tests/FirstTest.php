716c741460898dc47cf449f070abb53c6844aca9
trubiv <trubiv@gmail.com>
2024-12-12 02:30:31 +0300

    add phpunit

---
+++ tests/FirstTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class FirstTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
